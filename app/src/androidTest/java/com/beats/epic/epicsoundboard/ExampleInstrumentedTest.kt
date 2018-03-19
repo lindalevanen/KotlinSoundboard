37d1b239436f6ca9d4218d65e385a8bfaa3333a0
Levänen Linda <levanel2@cs.hut.fi>
2018-03-19 02:14:31 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/beats/epic/epicsoundboard/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.beats.epic.epicsoundboard
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.beats.epic.epicsoundboard", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/beats/epic/epicsoundboard/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.beats.epic.epicsoundboard
+
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.SoundPool
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val mAttributes: AudioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .build();
+
+    val mSoundPool: SoundPool =
+        SoundPool.Builder()
+                .setAudioAttributes(mAttributes)
+                .setMaxStreams(10)
+                .build()
+
+    var mDrumSoundId: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initSounds()
+        initSoundButtons()
+    }
+
+    private fun initSounds() {
+        mDrumSoundId = mSoundPool.load(this, R.raw.drum_sound, 1);
+    }
+
+    private fun initSoundButtons() {
+        for(i in 0..11) {
+            val sb = layoutInflater.inflate(R.layout.sound_button, soundBoardGrid, false)
+            sb.id = i
+            sb.setOnTouchListener { v, event ->
+                if(event.action == MotionEvent.ACTION_DOWN) {
+                    mSoundPool.play(mDrumSoundId, 1f, 1f, 0, 0, 1f)
+                    v.setBackgroundColor(Color.WHITE)
+                } else if(event.action == MotionEvent.ACTION_UP) {
+                    v.setBackgroundColor(Color.RED)
+                }
+                true
+            }
+            soundBoardGrid.addView(sb)
+        }
+    }
+}
